171cb2cf677670285cc6c02480a8e7e23096db13
Prince875799 <princekumar57987@gmail.com>
2026-05-28 18:20:41 +0530

    fixed https backend url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ai-career-backend-3sbg.vercel.app",
+  baseURL: "https://ai-career-backend-3sbg.vercel.app/api",
   withCredentials: true   // cookie send करने के लिए
 });
 
